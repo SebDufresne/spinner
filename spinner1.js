58fc5e397cf8e02a5d86904b3332959929483090
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-02 20:38:38 +0000

    Implemented spinner 2

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const spinner = ['\r|   ','\r/   ','\r-   ','\r\\   '];
+
+const displayString = (arr,count,repeat) => {
+  setTimeout(() => {
+    process.stdout.write(arr[count % 4]);
+    if (count < repeat) displayString(arr,++count,repeat);
+  }, 100);
+};
+
+displayString(spinner, 0, 100);
+
+/*
+100);
+300);
+500);
+700);
+*/
