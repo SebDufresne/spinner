ffebe78a5377db2ab6f92e33d091c17b4370f17f
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-02 20:42:05 +0000

    Added spinner1

--- spinner1.js
+++ spinner1.js
@@ -1,12 +1,35 @@
 'use strict';
 
-const spinner = ['\r|   ','\r/   ','\r-   ','\r\\   '];
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
 
-const displayString = (arr,count,repeat) => {
-  setTimeout(() => {
-    process.stdout.write(arr[count % 4]);
-    if (count < repeat) displayString(arr,++count,repeat);
-  }, 100);
-};
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
 
-displayString(spinner, 0, 100);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 1500);
